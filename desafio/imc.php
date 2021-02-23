dba450482f9e4c8163fb42defd5a313d6a8f1910
Vini Black <viniibl4ck@gmail.com>
2021-02-23 18:42:05 -0300

    desafios

---
+++ desafio/imc.php
@@ -0,0 +1,18 @@
+<?php
+
+$peso = 70;
+$altura = 1.77;
+
+$imc =  $peso / ($altura * $altura);
+
+
+echo "Seu IMC é de " . round($imc, 1). " Você está com IMC ";
+if ($imc < 18.5) {
+  echo "baixo";
+} else if ($imc < 25) {
+  echo "dentro";
+} else {
+  echo "acima";
+}
+
+echo "do recomendado";
---
+++ desafio/tabuada.php
@@ -0,0 +1,8 @@
+<?php
+
+$a = 2;
+
+for ($i = 1; $i <= 10; $i++) {
+  $result = $a * $i;
+  echo "$a * $i = $result" . PHP_EOL;
+}

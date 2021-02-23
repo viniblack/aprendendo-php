7ca52ba2af6e288cd497797cd017c92c34454f40
Vini Black <viniibl4ck@gmail.com>
2021-02-23 17:03:25 -0300

    aula 9 e 10

--- decisoes.php
+++ decisoes.php
@@ -1,17 +1,21 @@
 <?php
 
-$idade = 18;
-$nome = "Vinicius";
+$idade = 16;
+$numeroDePessoas = 2;
 
-echo "Você só pode entrar se tiver mais do que 18 anos." . PHP_EOL;
+echo "Você só pode entrar se tiver mais do que 18 anos ou a partir de 16 anos acompanhado" . PHP_EOL;
 
 if ($idade >= 18) {
-  echo "Você tem $idade anos." . PHP_EOL;
-  echo "Pode entrar";
+  echo "Você tem $idade anos. Pode entrar sozinho";
+} else if ($idade >= 16 && $numeroDePessoas >= 2) {
+  echo "Você tem $idade anos, mas esta acompanhado. Pode entrar";
 } else {
-  echo "Você tem $idade anos." . PHP_EOL;
-  echo "Não pode entrar";
+  echo "Você tem $idade anos e não esta acompanhado. Você não pode entrar.";
 }
+
+/*  Operador Ternário
+ $mensagem = $idade >= 16 && $numeroDePessoas >= 2  ? "Você tem $idade anos, mas esta acompanhado. Pode entrar" : "Você tem $idade anos e não esta acompanhado. Você não pode entrar.";
+*/
 
 echo  PHP_EOL;
 echo "Adeus";
